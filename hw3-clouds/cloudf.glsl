0644d69a8c7f1b158662bd0e9b085479e1e42c73
ilker <ilkerkosaroglu@gmail.com>
2023-06-02 00:55:36 +0300

    cotton field - 3d perlin

--- hw3-clouds/cloudf.glsl
+++ hw3-clouds/cloudf.glsl
@@ -6,20 +6,70 @@ in mat4 inverseViewingMatrix;
 in vec4 pos;
 out vec4 fragColor;
 
-float stepSize = 0.1;
-const int maxSteps = 500;
+float stepSize = 0.5;
+const int maxSteps = 1000;
 
 vec3 skyColor = vec3(0.2, 0.4, 0.69);
 vec3 cloudColor = vec3(1.0, 1.0, 1.0);
-float absorptionCoeff = stepSize * 1.0;
+float absorptionCoeff = stepSize * 0.20;
+
+vec3 gradients[16] = {
+	vec3(1, 1, 0),
+	vec3(-1, 1, 0),
+	vec3(1, -1, 0),
+	vec3(-1, -1, 0),
+	vec3(1, 0, 1),
+	vec3(-1, 0, 1),
+	vec3(1, 0, -1),
+	vec3(-1, 0, -1),
+	vec3(0, 1, 1),
+	vec3(0, -1, 1),
+	vec3(0, 1, -1),
+	vec3(0, -1, -1),
+	vec3(1, 1, 0),
+	vec3(-1, 1, 0),
+	vec3(0, -1, 1),
+	vec3(0, -1, -1)
+};
+
+int table[16] = {
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+};
+
+float f(float x){
+	x = abs(x);
+	if(x > 1.0) return 0.0;
+	float x2 = x * x;
+	float x4 = x2 * x2;
+	return -6.0 * x4 * x + 15.0 * x4 - 10.0 * x2 * x + 1.0;
+}
+
+
+
+vec3 getGradient(ivec3 p){
+	int idx;
+	idx = table[abs(p.z) % 16];
+	idx = table[abs(p.y + idx) % 16];
+	idx = table[abs(p.x + idx) % 16];
+	return gradients[idx];
+}
 
 float noiseOpacity(vec3 p){
-	// return fract(sin(dot(p, vec3(12.9898, 78.233, 45.5432))) * 43758.5453);
-	return abs(sin(p.x) + sin(p.y) + sin(p.z)) * 0.05;
-	// if(length(p) < 10.5){
-	// 	return 1.0;
-	// }
-	// return 0.0;
+	if(p.y < 0.0) return 0.0;
+	if(p.y > 10.0) return 0.0;
+
+	p *= 0.05;
+	ivec3 ijk = ivec3(p);
+	float c = 0.0;
+	for(int i=0;i<8;i++){
+		ivec3 di = ivec3(i / 4, (i / 2) % 2, i % 2);
+		ivec3 corner = ijk + di;
+		vec3 g = getGradient(corner);
+		vec3 d = vec3(p) - vec3(ijk);
+		float weight = f(d.x) * f(d.y) * f(d.z);
+		c += weight * dot(g, d);
+	}
+	return abs(c);
 }
 
 void main(void)
@@ -36,10 +86,14 @@ void main(void)
 	for(int i = 0; i < maxSteps; i++){
 		float opacity = noiseOpacity(rayPos);
 		float absorbed = opacity * absorptionCoeff;
+
 		transparency *= 1.0 - absorbed;
-		color = absorbed * cloudColor + (1.0 - absorbed) * color;
+
+		color = mix(cloudColor, color, absorbed);
 		rayPos -= rayDir * stepSize;
 	}
 
+	// float op = noiseOpacity(eyePos.xyz);
+	// fragColor = vec4(op,op,op, 1);
 	fragColor = vec4(color, 1-transparency);
 }
